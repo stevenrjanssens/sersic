d3266583d74ca88767ca1d4bdba44ca55e5ac63d
Steven Janssens <janssens@astro.utoronto.ca>
2017-10-10 23:02:27 -0400

    Turned into a proper package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+	name='sersic',
+	version='0.0.0dev',
+	description='Sersic profile utils',
+	author='Steven Janssens',
+	url='http://github.com/stevenrjanssens/sersic',
+	packages=find_packages('src'),
+	package_dir={'': 'src'}
+)
